a9b48a520e8e0a2351e5d2d4ab900a4b85e6f511
NarimanOusman <narimanrobo8@gmail.com>
2025-10-06 13:57:52 -0700

    Add link from landing page to home page

--- cham/urls.py
+++ cham/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Index, name='index'),
-    path('home', views.Home, name='home'),
+    path('home/', views.Home, name='home'),
     path('about/', views.About, name='about'),
     path('contact/', views.Contact, name='contact'),
     path('services/', views.Services, name='services'),
