a9b4717a2eb0972ea01f70d010c574eba74744d3
NarimanOusman <narimanrobo8@gmail.com>
2025-10-27 11:14:06 -0700

    added auth system

--- cham/admin.py
+++ cham/admin.py
@@ -1,3 +1,8 @@
+# cham/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+# Re-register UserAdmin to ensure 'is_active' is editable
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- cham/urls.py
+++ cham/urls.py
@@ -1,5 +1,7 @@
+# cham/urls.py
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.Index, name='index'),
@@ -8,4 +10,13 @@ urlpatterns = [
     path('contact/', views.Contact, name='contact'),
     path('services/', views.Services, name='services'),
     path('breeds/', views.Breeds, name='breeds'),
+
+    # Auth URLs
+    path('login/', views.user_login, name='login'),
+    path('signup/', views.user_signup, name='signup'),
+    path('logout/', views.user_logout, name='logout'),
+    path('password-reset/', views.password_reset, name='password_reset'),
+    path('verify-otp/', views.verify_otp, name='verify_otp'),
+    path('reset-password/', views.reset_password, name='reset_password'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
