458ed22a4dbf07f358685db4318ecc7397310d05
NarimanOusman <narimanrobo8@gmail.com>
2025-10-27 12:44:47 -0700

    removed auth

--- cham/urls.py
+++ cham/urls.py
@@ -11,12 +11,5 @@ urlpatterns = [
     path('services/', views.Services, name='services'),
     path('breeds/', views.Breeds, name='breeds'),
 
-    # Auth URLs
-    path('login/', views.user_login, name='login'),
-    path('signup/', views.user_signup, name='signup'),
-    path('logout/', views.user_logout, name='logout'),
-    path('password-reset/', views.password_reset, name='password_reset'),
-    path('verify-otp/', views.verify_otp, name='verify_otp'),
-    path('reset-password/', views.reset_password, name='reset_password'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    
 ]
