dbacfedd84f7c4859df5cf2a8e29cdca4d87e0d5
NarimanOusman <narimanrobo8@gmail.com>
2025-11-02 01:05:37 -0700

    restore

--- cham/admin.py
+++ cham/admin.py
@@ -1,8 +1,1 @@
 # cham/admin.py
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
-
-# Re-register UserAdmin to ensure 'is_active' is editable
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
